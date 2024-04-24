d9eeb7e489fc2e0e4be31981bea6f4cefa1677b1
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-24 09:49:32 +0000

    data types

---
+++ 01_basics/02_data-types.js
@@ -0,0 +1,26 @@
+"use strict "// we are using new version of js only in our file
+
+// alert(7+7) //we are working on node js not on browser ..it is available on node but not this ways
+
+// make code Readable
+// console.log(9+8) ;console.log(8+)
+// console.log(9
+    
+//     +8) console.log(8+);
+// tc39.is
+// mdn documentaions
+
+
+// primitive data types
+
+// number 2 to power 58
+// biginit
+// string "" ''
+// Boolean   true /false
+// undefined
+// null - shows presence of nothing -standalone value
+// symbol - uniquw
+
+ console.log(typeof null ); //object 
+ console.log(typeof undefined); //undefined
+ console.log(typeof [undefined]); //object
