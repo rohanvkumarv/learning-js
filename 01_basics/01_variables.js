1aa5519926b85c9b8c6b3dc6afc9f10d9ee99ee0
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-24 09:19:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const account_id = 1
+let account_email = "rohanv@gmail.com"
+
+
+var account_password = "rohan"
+// account_city = "agra"  never use this  
+
+let status; 
+// ; are optional 
+
+// account_id =02 not allowed
+console.log(account_id);
+account_email = "raj@"
+account_password= "990"
+account_city  ="banaras"
+console.table([account_email,account_password,account_city])
+
+/*
+prefer not to use var - because of issue in block scope and fuction scope
+*/
