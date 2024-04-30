a587cf903425bb3925e9c939907fb0730b1a4e54
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-30 09:11:57 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// basics 
+// console.log(1>2);
+// console.log(1==2);
+// console.log(1<2);
+// console.log(1<=2);
+// console.log(1>=2);
+// console.log(1!=2);
+
+
+// console.log("2">1);
+// console.log("022">1); converting to num here 
+
+// console.log(null >1);
+// console.log(null ==1);
+// console.log(null <= 1);
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined!=0);
+
+// avoid  above  types of conversions
+
+// comparison and  equality is diff in js
+
+
+// strict check  === check value and dataatyep
+
+// console.log("2"==2);
+
+// console.log("2"===2);
