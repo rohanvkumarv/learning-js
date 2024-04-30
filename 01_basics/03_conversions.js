57c61709e053924a077a649443570bb1a8a39708
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-30 08:55:12 +0000

    operations

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -37,4 +37,71 @@
 // console.log(string_sum);
 // console.log(typeof string_sum);
 
-// operations
+
+
+// ................operations......................
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+// let str1 ="hello"
+// let str2 = " raj"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+2)
+// console.log(1+2+"2")
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3 
+// num1=num2=num2 =2+2
+// let gamecounter = 100
+// // gamecounter++;
+// ++gamecounter;
+// console.log(gamecounter);
+
+// eg 
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+let a = 4;
+a++
+console.log(a);
+let y= 4;
+++y
+console.log(y)
+
+
+
+
+
+
+
+
+
+
+
