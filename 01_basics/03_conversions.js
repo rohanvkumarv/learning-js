576d7b78cce945b4c9a5e5eccdea702adce58974
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-30 08:11:33 +0000

    conversion of data types

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,40 @@
+// let score = 33
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let scoree = "33"
+// console.log(typeof scoree)
+
+
+// var number_score = Number(scoree)
+// console.log(typeof number_score)
+
+// let scoreee = "33abc" //nan values
+// let scoreee = null //0
+// let scoreee = undefined //nan
+// let scoreee = true //1
+// let scoreee ="true" //nan
+// var number_scoree = Number(scoreee)
+// console.log(typeof number_scoree)
+// console.log(number_scoree)
+
+
+
+// let isloggedin = 1
+// // let isloggedin = ""
+// // let isloggedin = "hitesh"
+// let converted = Boolean(isloggedin)
+// console.log(typeof converted);
+// console.log(converted);
+
+
+// 1= true ;0  = false
+// "" = false ; "name" = true
+
+
+// let sum = 33
+// let string_sum = String(sum)
+// console.log(string_sum);
+// console.log(typeof string_sum);
+
+// operations
