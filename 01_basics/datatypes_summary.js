420775198295d6caa7830a2f7334221bfb0de386
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-04-30 09:35:44 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,52 @@
+// primitive and non premetivie
+// call by valye and Reference
+// how they arr stored and access
+
+// primitive data types
+
+
+// not original value is given only a copy of valeu is given
+// 7 types :// String,
+            // number 
+            // boolean 
+            // null 
+            // undefined
+            // symbol 
+            // bigint 
+
+
+// const score =100;
+// const sc_value = 100.3
+// const isloggedin = false
+// const outtemp = null  retuns object
+// let userEmail = undefined;
+
+
+// const id = Symbol  ('123')
+// const anotherid = Symbol('123')
+// console.log(id);
+// console.log(anotherid);
+// console.log(anotherid ==id);
+// console.log(anotherid ===id);
+
+// const bignumber = 98233839912980199902n
+// console.log(typeof bignumber);
+
+
+//  non premetive  (reference )           
+            // arrays 
+            // objects
+            // functions
+
+// const heroes = ["shaktiman", "nagraj","doga"]
+// let myobj = {
+//     name:"raj",
+//     age:222,
+// }
+
+// const myfunction = function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof myobj);
+// myfunction()
