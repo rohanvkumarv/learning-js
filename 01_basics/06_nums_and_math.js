861b02e513e80f3fea653b3a07093afc0a5362ad
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-17 05:12:58 +0000

    06_nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score =100;
+const balance = new Number (100);
+console.log(balance);
+// if u r telling its a num it will  explicitly tell too 
+console.log(score);
+// prototype properties
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const anothernum = 23.8992;
+console.log(anothernum.toPrecision(3));
+// gives round of value to specified number of digits 
+const hundreds = 100000000;
+console.log(hundreds.toLocaleString('en-In'));
