104cc9f6ce46165affa0a656dcf4910d9791d1c0
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-18 17:28:19 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// array is obj in js 
+
+const myArr  = [1,3,4,2,true,"raj"]
+// heterogenous 
+// reziiable
+// index starts by zero 
+console.log(myArr[0]);
+console.log(myArr["one"]); // not allowed 
+// and copy like by refrece hota copy nhi bant ahua  
+
+// ways to declare 
+const arr = ["iron man  " , "dr.strange"]
+const arr1 = new Array("iron man  " , "dr.strange")
+
+
+// array methods 
