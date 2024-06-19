371636fa5fa709dbe979c71ea9b01fd8221646b6
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-19 06:53:05 +0000

    array basics

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,13 +4,47 @@ const myArr  = [1,3,4,2,true,"raj"]
 // heterogenous 
 // reziiable
 // index starts by zero 
-console.log(myArr[0]);
-console.log(myArr["one"]); // not allowed 
+// console.log(myArr[0]);
+// console.log(myArr["one"]); // not allowed 
 // and copy like by refrece hota copy nhi bant ahua  
 
 // ways to declare 
-const arr = ["iron man  " , "dr.strange"]
+const arr = ["iron man  " , "dr.strange",6,8,91,20,12,3,]
 const arr1 = new Array("iron man  " , "dr.strange")
 
 
-// array methods 
+// // array methods 
+// arr.push(6)
+// arr.push("Raj")  /// add element at last posiiotn
+// arr.pop() //removes last elemenet
+// arr.unshift(0) //inserts element at start position not recommeneded
+// arr.shift()
+
+// console.log(arr.includes(6));// returns a vooleans type  
+// console.log(arr.indexOf(9));
+// console.log(arr.indexOf(6));
+
+
+// const newarr = arr.join()
+// console.log(arr);
+// console.log(typeof newarr);
+
+// slice splice 
+
+console.log("a",arr);
+
+const mya1  = arr.slice(1,3)
+console.log(mya1);
+
+console.log("b",arr);
+
+
+
+
+// splice 
+const mya2 = arr.splice(1,3)
+console.log("c" ,arr);
+console.log(mya2);
+
+// slice doesnt change original array 
+// splice cuts those elements 
