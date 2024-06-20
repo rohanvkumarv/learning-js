a5c4a3b82e13c226bf3377a2183faec2038a397b
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-20 17:30:58 +0000

    objects 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,4 +22,61 @@ const regularUser = {
 
 // nesting allowed of objects too 
 
-console.log(regularUser.fullname);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userFullname.firstname);
+// console.log(regularUser.fullname?.userFullname.firstname); //if fullname doenst exits 
+
+
+// objects merge or combine  
+
+const obj1 = {1:"a" ,2:"b" }
+
+const obj2 = {3:"c" ,4:"d" }
+
+const obj4 = {5:"c" ,6:"d" }
+
+// const obj3 = {obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2,obj4)
+// here every object goes to empty object 
+// if we dont use that it goes to first obj all other objs  
+
+const obj5 = {...obj1,...obj3}
+
+
+// console.log(obj5);
+
+
+const users = [
+    {
+        id:"1",
+        name:"rohan"
+
+    },
+    {
+        id:"1",
+        name:"rohan"
+
+    },
+    {
+        id:"1",
+        name:"rohan"
+
+    },
+    {
+        id:"1",
+        name:"rohan"
+
+    }
+
+]
+
+// users[0].name
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser)); // returns array 
+// console.log(Object.values(tinderUser)); // returns array 
+// console.log(Object.entries(tinderUser)); // returns array
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
