abd632eebf14594002e4e9ee7dce89bd80eaeb28
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-21 10:15:26 +0000

    scope lec 2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -11,15 +11,15 @@ if (true){
     let  a =10
     const b=20
     var  c=30
-    console.log(a);
+    // console.log(a);
 }
 
 
 
 
-console.log(a);   //a is  not defined
+// console.log(a);   //a is  not defined
 // console.log(b);   //b is not defined
-console.log(c);     // defines variable in global scope
+// console.log(c);     // defines variable in global scope
 
 
 // { //scope,block, not in obj
@@ -32,3 +32,48 @@ console.log(c);     // defines variable in global scope
 
 
 // node and browser scope is diff 
+
+
+
+// second lecturee 
+
+function one(){
+    const username = "hitesh"
+    function two  (){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+    // two()
+}
+// in nested scopes child can accept values from parent not vice versa /
+one()
+
+
+
+
+
+if (true){
+    const username ="hitesh";
+    if (username === "hitesh"){
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);.
+
+
+// ............interesting  ...................... .... .
+
+// two types of function declarations 
+// hoisting   
+console.log(addone(5))
+function addone (num) {
+    return num+1;
+    
+}
+console.log(addtwo(5))
+const addtwo = function (num){
+    return num+2;
+}
