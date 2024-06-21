ab5dac27e09858a0f99ce3bf2eefb2d8f6a6f5aa
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-21 09:41:28 +0000

    scope 1

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,34 @@
+// let  a =10
+// const b=20
+// var  c=30
+
+
+
+// var c =3000; defines in global space  
+let a=220; // jaha define karo vahi par chalega variable 
+// const b=200;
+if (true){
+    let  a =10
+    const b=20
+    var  c=30
+    console.log(a);
+}
+
+
+
+
+console.log(a);   //a is  not defined
+// console.log(b);   //b is not defined
+console.log(c);     // defines variable in global scope
+
+
+// { //scope,block, not in obj
+
+// block scope  
+// variable defined in block shouldnt go outside vice versa is not same 
+
+// }
+// global scope 
+
+
+// node and browser scope is diff 
