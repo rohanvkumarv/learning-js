ab8a33218f33d72909cd76ec51d184c6ad862ba1
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-06-29 18:01:52 +0000

    oops -object start

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -3,6 +3,27 @@
 const user = {
     username : "hitesh",
     loginCount :8,
-    signedIn : true
+    signedIn : true,
+    GetUserDetails:function(){
+        // console.log(`username ${this.username}`);
+        console.log(this);
+    }
 }
-console.log(user.username);
+
+// console.log(user.username);
+// console.log(user.GetUserDetails());
+
+// console.log(this);
+
+// constructor function 
+
+
+function User (username,loginCount,isloggedin){
+   this.username = username
+   this.loginCount = loginCount
+   this.isloggedin = isloggedin
+
+   return this
+}
+
+const userone
