733e6fde7e589167f928f12543501b1376f6fb4d
Rohan kumar <114327254+rohanvkumarv@users.noreply.github.com>
2024-07-01 16:24:03 +0000

    how to change value of pi

---
+++ 10_classes_and_oops/mathpy.js
@@ -0,0 +1,36 @@
+const Descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+
+// console.log(Descriptor);
+
+// we can make our pi value 
+// const MynewObject = Object.create(n)
+
+const chai = {
+    name :"Ginger Chai",
+    price:150,
+    isAvailable:true,
+
+    OrderChai : function(){
+        console.log("chai nhi bani");
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai,"name",{
+    // writable:false,
+    enumerable: false,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key,value] of Object.entries(chai)) {
+    if (typeof value !=='function') {
+        console.log(`${key} ,${value}`);
+    }
+}
